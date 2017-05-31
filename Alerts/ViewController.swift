4bc664ee96a2c99e065c41034d95075ced75c2e2
jose ramirez <joseramirez@joses-MacBook-Pro.local>
2017-05-31 17:14:36 -0400

    Displayed a simple alert message on the screen when pressing a button creating an alert takes 4 steps laid out in the comments

--- Alerts/ViewController.swift
+++ Alerts/ViewController.swift
@@ -14,7 +14,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    ///serveral ways 1.) annotated through out code and commented out so that 2.) can be shown
+    
+    /*connected show alert button from the storyboard: in order to create an alert
+                                                       there are going to be 4 steps
+    step1: create an alert controller== which is the manager or you can think of it as
+           the whole unit
+           the whole alert or 
+           the whole pop up that we see
+    step2: then we need to create an Alert action == the actions are the buttons with the connected responses when that button is tapped
+    step3: then you need to connect the controller to the action
+    step4: then you need to present the alert
+ */
+    @IBAction func showAlert(_ sender: Any) {
+        /*STEP 1:
+        --note title and message can be nil, sometimes you just want to present a tittle sometimes you want both
+        */
+        let alert:UIAlertController = UIAlertController(title: "Tittle", message: "Message", preferredStyle: .alert)
+        /*STEP 2: create an action
+         */ //note:UIAlertAction in handeler is handled with an unnamed parameter that is an underscore _:UIAlertActionbelow
+        let action1:UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { (_:UIAlertAction) in
+            print("Cancel")
+        }
+        /*STEP3: Connect action to alert*/
+        alert.addAction(action1)
+        /*STEP4: present the view */
+        self.present(alert,animated: true){
+            print("Present complete")
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
